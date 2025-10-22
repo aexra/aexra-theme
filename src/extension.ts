947bfa5bf2b796d669daab5ab80595a87edb06b6
Никита Фомин <defdXs@yandex.ru>
2025-10-22 20:42:27 +0300

    7u edfvh gbyhg7ujyhtgbvyhbhwwh

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// let accentColorStatusBarItem: vscode.StatusBarItem;
+
+// export function activate(context: vscode.ExtensionContext): void {
+// 	// Create status bar accent color changer
+// 	accentColorStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+
+// 	// Add subscriptions
+// 	context.subscriptions.push(accentColorStatusBarItem);
+
+// 	// Initialize status bar status
+// 	updateStatusBarItem();
+// }
+
+// function updateStatusBarItem(): void {
+// 	accentColorStatusBarItem.text = "Abobabebebe";
+// 	accentColorStatusBarItem.show();
+// }
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "aexra-theme" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('aexra-theme.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Aexra Theme!');
-	});
-
-	context.subscriptions.push(disposable);
+	const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	item.text = "Abobabebebe";
+	item.show();
+	context.subscriptions.push(item);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
