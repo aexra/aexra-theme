c90adcd2010eb37c1e34dbd769dcc6f868328b9b
Никита Фомин <defdXs@yandex.ru>
2025-10-22 20:42:52 +0300

    etgrfhrftgbcdxrtghtgbxh

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,19 @@
 import * as vscode from 'vscode';
 
-// let accentColorStatusBarItem: vscode.StatusBarItem;
+let accentColorStatusBarItem: vscode.StatusBarItem;
 
-// export function activate(context: vscode.ExtensionContext): void {
-// 	// Create status bar accent color changer
-// 	accentColorStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+export function activate(context: vscode.ExtensionContext): void {
+	// Create status bar accent color changer
+	accentColorStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
 
-// 	// Add subscriptions
-// 	context.subscriptions.push(accentColorStatusBarItem);
+	// Add subscriptions
+	context.subscriptions.push(accentColorStatusBarItem);
 
-// 	// Initialize status bar status
-// 	updateStatusBarItem();
-// }
+	// Initialize status bar status
+	updateStatusBarItem();
+}
 
-// function updateStatusBarItem(): void {
-// 	accentColorStatusBarItem.text = "Abobabebebe";
-// 	accentColorStatusBarItem.show();
-// }
-
-export function activate(context: vscode.ExtensionContext) {
-	const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	item.text = "Abobabebebe";
-	item.show();
-	context.subscriptions.push(item);
+function updateStatusBarItem(): void {
+	accentColorStatusBarItem.text = "Abobabebebe";
+	accentColorStatusBarItem.show();
 }
